112052681d1822181ac44082b2f9dd000ece9edc
Ivan Rael Nuñez Harper <ivan.nunez@tectijuana.edu.mx>
2017-04-30 18:59:06 -0700

    accesses in mvc

---
+++ RAC/Controllers/AccessesController.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using RAC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace RAC.Controllers
+{
+    public class AccessesController : Controller
+    {
+        string connection;
+        public AccessesController()
+        {
+            connection = "http://ec2-54-213-8-236.us-west-2.compute.amazonaws.com/Accesses/";
+
+            /* to test post method
+            connection = "http://localhost:50509/Accesses/";
+            /**/
+        }
+
+        // GET: Accesses
+        [HttpGet]
+        public JsonResult Index()
+        {
+            using (WebClient wb = new WebClient())
+            {
+
+                string site = "";
+
+                string response = wb.DownloadString(connection + site);
+
+                List<AccessVM> accesses = JsonConvert.DeserializeObject<List<AccessVM>>(response);
+
+                return Json(accesses, JsonRequestBehavior.AllowGet);
+            }
+        }
+        // Create an access
+        [HttpPost]
+        public JsonResult Create(AccessVM accessVm)
+        {
+            accessVm.Date = DateTime.Now.ToString();
+
+            /* to test post method
+            accessVm.User = new UserVM()
+            {
+                IdUser = 0
+            };
+            accessVm.Area = new AreaVM()
+            {
+                IdArea = 1
+            };
+            /* */
+
+            JavaScriptSerializer JSS = new JavaScriptSerializer();
+
+            string site = "Create";
+            var wr = (HttpWebRequest)WebRequest.Create(connection + site);
+            wr.Method = "POST";
+            wr.ContentType = "application/json; charset=utf-8";
+
+            using (var streamWriter = new StreamWriter(wr.GetRequestStream()))
+            {
+                string json = JSS.Serialize(accessVm);
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+            var httpResponse = (HttpWebResponse)wr.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var responseText = streamReader.ReadToEnd();
+
+                responseText = JsonConvert.DeserializeObject<string>(responseText);
+
+                return Json(responseText, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
